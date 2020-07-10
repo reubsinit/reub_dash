d3d65428b54e20d967608d9da661ce1dffadcdad
reubsinit <reubenmwilson@gmail.com>
2020-07-10 13:44:15 +1000

    docs: jsdoc for every

--- every.js
+++ every.js
@@ -1,4 +1,21 @@
-function every(array, cb) {
+/**
+ * Every callback.
+ *
+ * @callback everyCallback
+ * @param {any} element - The current element being processed in the array.
+ * @param {number} idx - The index of the current element.
+ * @param {array} array - The array being iterated on.
+ */
+
+/**
+ * Tests whether all elements in the array pass the test implemented by the
+ * provided function. It returns a Boolean value.
+ * @param {array} array - The array to iterate over.
+ * @param {everyCallback} callback - A function to test each element.
+ * @return {boolean} `true` if the callback function returns a truthy value
+ *  for every array element. Otherwise, `false`.
+ */
+function every(array, callback) {
   for (let i = 0; i < array.length; i++) {
     if (!cb(array[i], i, array)) {
       return false;
@@ -6,3 +23,5 @@ function every(array, cb) {
   }
   return true;
 }
+
+export default every;
