839f27b06fe7698e108e4187972ae904e638f146
reubsinit <reubenmwilson@gmail.com>
2020-07-10 15:16:03 +1000

    docs: jsdocs for filter

--- flat.js
+++ flat.js
@@ -1,15 +1,27 @@
-function flat(array, depth) {
+/**
+ * Creates a new array with all sub-array elements concatenated into it
+ * recursively up to the specified depth.
+ * @param {array} array - The array to flatten.
+ * @param {number} [depth] - The depth level specifying how deep a
+ * nested array structure should be flattened. Defaults to 1.
+ * @return {array} A new array with the sub-array elements concatenated into it.
+ */
+export function flat(array, depth) {
   const result = [];
   let level = 0;
   depth = depth || 1;
+  /**
+   * Function for flattening an array.
+   * @param {array} array - The array to flatten.∂
+   */
   function flat(array) {
     for (let i = 0; i < array.length; i++) {
       const val = array[i];
       const isArray = Array.isArray(val);
-      if (isArray && level == depth || !isArray) {
+      if (!isArray || isArray && level == depth) {
         result.push(val);
       } else {
-        level++
+        level++;
         flat(val);
         level--;
       }
@@ -18,3 +30,5 @@ function flat(array, depth) {
   flat(array);
   return result;
 }
+
+export default flat;
