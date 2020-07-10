96ec5bcaf66251ef8e43796678f0ef8607b77b82
reubsinit <reubenmwilson@gmail.com>
2020-07-10 15:24:26 +1000

    docs: jsdocs for map

--- map.js
+++ map.js
@@ -1,7 +1,28 @@
-function map(array, cb) {
-  let result = [];
+/**
+ * map callback.
+ *
+ * @callback mapEachCallback
+ * @param {any} element - The current element being processed in the array.
+ * @param {number} [idx] - The index of the current element.
+ * @param {array} [array] - The array being iterated on.
+ */
+
+/**
+ * Creates a new array populated with the results of calling a provided
+ * function on every element in the calling array.
+ * @param {array} array - The array to iterate over.
+ * @param {mapEachCallback} callback - Function that is called for every
+ * element of arr. Each time callback executes, the returned value is added to
+ * new_array.
+ * @return {array} A new array with each element being the result of the
+ * callback function.
+ */
+function map(array, callback) {
+  const result = [];
   for (let i = 0; i < array.length; i++) {
-    result.push(cb(array[i], i, array));
+    result.push(callback(array[i], i, array));
   }
   return result;
 }
+
+export default map;
