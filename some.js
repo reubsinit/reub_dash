329e2beb1205deb6b9d9ce4c29b138c3636806b4
reubsinit <reubenmwilson@gmail.com>
2020-07-10 15:37:19 +1000

    docs: jsdocs for some

--- some.js
+++ some.js
@@ -1,8 +1,28 @@
-function some(array, cb) {
+/**
+ * some callback.
+ *
+ * @callback someCallback
+ * @param {any} element - The current element being processed in the array.
+ * @param {number} [idx] - The index of the current element.
+ * @param {array} [array] - The array being iterated on.
+ */
+
+/**
+ * Tests whether at least one element in the array passes the test implemented
+ * by the provided function. It returns a Boolean value.
+ * @param {array} array - The array to iterate over.
+ * @param {someCallback} callback - Function to execute on each value
+ * in the array.
+ * @return {boolean} `true` if the callback function returns a truthy
+ * value for at least one element in the array. Otherwise, `false`.
+ */
+function some(array, callback) {
   for (let i = 0; i < array.length; i++) {
-    if (cb(array[i], i, array)) {
+    if (callback(array[i], i, array)) {
       return true;
     }
   }
   return false;
 }
+
+export default some;
