c1b3ce0d215c96c40bb5268be0f786180fdc4868
reubsinit <reubenmwilson@gmail.com>
2020-07-10 15:10:06 +1000

    docs: jsdoc for find

--- find.js
+++ find.js
@@ -1,4 +1,22 @@
-function find(array, cb) {
+/**
+ * Find callback.
+ *
+ * @callback findCallback
+ * @param {any} element - The current element being processed in the array.
+ * @param {number} [idx] - The index of the current element.
+ * @param {array} [array] - The array being iterated on.
+ */
+
+/**
+ * Returns the value of the first element in the provided array that satisfies
+ * the provided testing function..
+ * @param {array} array - The array to iterate over.
+ * @param {findCallback} callback - Function to execute on each value in the
+ * array.
+ * @return {any} The value of the first element in the array that satisfies
+ * the provided testing function. Otherwise, undefined is returned.
+ */
+function find(array, callback) {
   for (let i = 0; i < array.length; i++) {
     if (cb(array[i], i, array)) {
       return array[i];
@@ -6,3 +24,5 @@ function find(array, cb) {
   }
   return undefined;
 }
+
+export default find;
