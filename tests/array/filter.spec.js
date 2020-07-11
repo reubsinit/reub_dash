5e3c30bf8671d72f54e7c4fcbeb9c98ff9818525
reubsinit <reubenmwilson@gmail.com>
2020-07-11 20:15:51 +1000

    quality: improve logic

--- tests/array/filter.spec.js
+++ tests/array/filter.spec.js
@@ -2,40 +2,27 @@ import filter from '../../src/array/filter';
 
 /* eslint-disable require-jsdoc */
 const words = ['spray', 'limit', 'exuberant', 'destruction', 'elite', 'present'];
-let toFilter;
-let rd;
-let nativeFilter;
 
-beforeEach(() => {
-  toFilter = undefined;
-});
+function testFunction(toFilter, cb, mutate = false) {
+  const rd = filter(!mutate ? toFilter : [...toFilter], cb);
+  const nativeFilter = toFilter.filter(cb);
+
+  expect(rd).toEqual(nativeFilter);
+}
 
 describe('filter', () => {
   it('should filter each word by word.length > 6', () => {
     function greaterThan6(word) {
       return word.length > 6;
     }
-
-    toFilter = words;
-
-
-    rd = filter(toFilter, greaterThan6);
-    nativeFilter = toFilter.filter(greaterThan6);
-
-    expect(rd).toEqual(nativeFilter);
+    testFunction(words, greaterThan6);
   });
 
   it('should filter each number >= 10', () => {
     function isBigEnough(value) {
       return value >= 10;
     }
-
-    toFilter = [12, 5, 8, 130, 44];
-
-    rd = filter(toFilter, isBigEnough);
-    nativeFilter = toFilter.filter(isBigEnough);
-
-    expect(rd).toEqual(nativeFilter);
+    testFunction([12, 5, 8, 130, 44], isBigEnough);
   });
 
   it('should filter all prime numers', () => {
@@ -47,13 +34,7 @@ describe('filter', () => {
       }
       return num > 1;
     }
-
-    toFilter = [-3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
-
-    rd = filter(toFilter, isPrime);
-    nativeFilter = toFilter.filter(isPrime);
-
-    expect(rd).toEqual(nativeFilter);
+    testFunction([-3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13], isPrime);
   });
 
   it('should filter invalid entries from JSON', () => {
@@ -76,31 +57,17 @@ describe('filter', () => {
       {id: 'undefined'},
     ];
 
-    rd = filter(toFilter, filterByID);
-    nativeFilter = toFilter.filter(filterByID);
-
-    expect(rd).toEqual(nativeFilter);
+    testFunction(toFilter, filterByID);
   });
 
   it('should search an arry', () => {
-    function nativeFilter(array, query) {
-      return array.filter(function(ele) {
+    function cb(query) {
+      return function(ele) {
         return ele.toLowerCase().indexOf(query.toLowerCase()) !== -1;
-      });
+      };
     }
 
-    function rdFilter(array, query) {
-      return filter(array, function(ele) {
-        return ele.toLowerCase().indexOf(query.toLowerCase()) !== -1;
-      });
-    }
-
-    toFilter = ['apple', 'banana', 'grapes', 'mango', 'orange'];
-
-    rd = rdFilter(toFilter, 'an');
-    nativeFilter = nativeFilter(toFilter, 'an');
-
-    expect(rd).toEqual(nativeFilter);
+    testFunction(['apple', 'banana', 'grapes', 'mango', 'orange'], cb('an'));
   });
 
   it('should modify array items', () => {
@@ -109,10 +76,7 @@ describe('filter', () => {
       return word.length < 6;
     }
 
-    rd = filter([...words], cb);
-    nativeFilter = [...words].filter(cb);
-
-    expect(rd).toEqual(nativeFilter);
+    testFunction(words, cb, true);
   });
 
   it('should append new array items', () => {
@@ -121,10 +85,7 @@ describe('filter', () => {
       return word.length < 6;
     }
 
-    rd = filter([...words], cb);
-    nativeFilter = [...words].filter(cb);
-
-    expect(rd).toEqual(nativeFilter);
+    testFunction(words, cb, true);
   });
 
   it('should delete array items', () => {
@@ -133,9 +94,6 @@ describe('filter', () => {
       return word.length < 6;
     }
 
-    rd = filter([...words], cb);
-    nativeFilter = [...words].filter(cb);
-
-    expect(rd).toEqual(nativeFilter);
+    testFunction(words, cb, true);
   });
 });
