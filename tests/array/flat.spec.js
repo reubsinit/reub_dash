0dc37350b859d3019ad84387c317fe5322f521ee
reubsinit <reubenmwilson@gmail.com>
2020-07-12 11:54:49 +1000

    test: add tests for flat

---
+++ tests/array/flat.spec.js
@@ -0,0 +1,15 @@
+import {compare} from '../helpers';
+
+describe('flat', () => {
+  it('should flatten array with depth of 1', () => {
+    compare('flat', [1, 2, [3, 4, [5, 6]]]);
+  });
+
+  it('should flatten array with depth of 2', () => {
+    compare('flat', [1, 2, [3, 4, [5, 6]], [[7, 8, [[10]]]]], 2);
+  });
+
+  it('should flatten array with depth of infinity', () => {
+    compare('flat', [1, 2, [3, 4, [5, 6]], [[7, 8, [[10]]]]], Infinity);
+  });
+});
