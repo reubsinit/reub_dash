023c6fd8ac61d5e13401dd08d3f30a6f9903baa0
reubsinit <reubenmwilson@gmail.com>
2020-07-12 11:20:57 +1000

    enhance: export array methods

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import every from './array/every';
+import filter from './array/filter';
+import find from './array/find';
+import findIndex from './array/findIndex';
+import flat from './array/flat';
+import flatMap from './array/flatMap';
+import forEach from './array/forEach';
+import map from './array/map';
+import reduce from './array/reduce';
+import reduceRight from './array/reduceRight';
+import some from './array/some';
+
+export default {
+  every,
+  filter,
+  find,
+  findIndex,
+  flat,
+  flatMap,
+  forEach,
+  map,
+  flatMap,
+  reduce,
+  reduceRight,
+  some,
+};
